5f666c7af173033272e076db8bc8675e2819c061
wildpioneer <wildpioneer@yandex.ru>
2024-06-20 20:52:31 +0200

    Updated

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,14 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int div(int a, int b) {
+        return a + b;
+    }
+
+    public double div(double a, double b) {
+        return a + b;
+    }
+
+
 }
