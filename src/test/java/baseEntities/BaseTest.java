485745c6050c2cc4a22e499cffac4a269f0233f4
wildpioneer <wildpioneer@yandex.ru>
2024-07-23 20:20:37 +0200

    Updated

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,19 +1,26 @@
 package baseEntities;
 
+import configuration.ReadProperties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import services.BrowsersService;
 import services.WaitsService;
+import steps.UserStep;
 
 public class BaseTest {
     protected WebDriver driver;
+    protected UserStep userStep;
     protected WaitsService waitsService;
 
     @BeforeMethod
     public void setup() {
         driver = new BrowsersService().getDriver();
         waitsService = new WaitsService(driver);
+
+        userStep = new UserStep(driver);
+
+        driver.get(ReadProperties.getUrl());
     }
 
     @AfterMethod
---
+++ src/test/java/steps/ProjectStep.java
@@ -0,0 +1,12 @@
+package steps;
+
+public class ProjectStep {
+
+    public void addProject(String projectName) {}
+
+    public void editProject(String projectName) {}
+
+    public void searchProject(String projectName) {}
+
+    public void deleteProject(String projectName) {}
+}
