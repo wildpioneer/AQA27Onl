ebf09affc78b003b2eeb356b0f767f005f631d23
wildpioneer <wildpioneer@yandex.ru>
2024-08-15 19:36:18 +0200

    Updated

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -12,17 +12,6 @@ import steps.UserStep;
 public class LoginTest extends BaseTest {
 
     @Test
-    public void successLoginTest() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.setEmailValue(ReadProperties.username());
-        loginPage.setPasswordValue(ReadProperties.password());
-        loginPage.clickLogin();
-
-        DashboardPage dashboardPage = new DashboardPage(driver);
-        Assert.assertTrue(dashboardPage.isPageOpened());
-    }
-
-    @Test
     public void shortSuccessLoginTest() {
         Assert.assertTrue(
                 userStep.successfulLogin(ReadProperties.username(), ReadProperties.password())
@@ -36,17 +25,5 @@ public class LoginTest extends BaseTest {
                         .getErrorTextElement().getText(),
                 "Email/Login or Password is incorrect. Please try again.",
                 "Неверное сообщение об ошибке");
-    }
-
-    @Test
-    public void incorrectPswLoginTest() {
-/*
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(ReadProperties.username(), "asdas");
-
-        Assert.assertEquals(loginPage.getErrorTextElement().getText(),
-                "Email/Login or Password is incorrect. Please try again.",
-                "Неверное сообщение об ошибке");
-*/
     }
 }
